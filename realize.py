4e99d7081ccb345e5eb6988bc8476b84c917c19a
Ryan Breaker <rynbreaking@gmail.com>
2012-08-11 01:34:37 -0500

    Added to realize.py and including random testing file

--- realize.py
+++ realize.py
@@ -2,7 +2,27 @@
 # Would include:
 #   [0,0],[0,1],[0,2],[0,3],[0,4],[0,5]
 #   [0,0],[1,0],[2,0],[3,0],[4,0],[5,0]
-#   
+#   		
 
 def realize(coords):
+	"""
+	"realize" = to find all of the coords inside an area
+	Finds them by laying-out the coords in coordL, getting the
+	lowest and highest elements, and returning the possible
+	coords
+	"""
+    e = 0 # element number
+    coordsL = [] # Coords 'Laid'-out
+    for i in coords: # lays out coords to get lowest/highest nums
+		for ii in coords[e]:
+			coordsL.append(ii)
+		e += 1
+	
+    coordsL.sort()
+    l = coordsL[0] # lowest
+    h = coordsL[7] # highest
     
+    if l > h or l == h: # sanity check
+		exit("ERROR: In realize")
+	
+	# adding more Saturday
---
+++ test.py
@@ -0,0 +1,10 @@
+### TESTING FILE ###
+
+coords = [[1,2],[3,4],[5,6],[7,8]]
+
+el = 0
+
+for i in coords:
+	for ii in coords[el]:
+		print ii
+	el += 1
